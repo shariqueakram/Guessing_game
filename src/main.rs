17a239429649b005a5de5f937b5a1ca9570cf6e0
Sharique Akram <shariqueakram@gmail.com>
2025-02-05 20:30:03 -0500

    Removed print which shows Secret number during debugging

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,7 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=21);
     //range behavior is the same as python
     //adjust the range here
-    
-    println!("The secret number is: {secret_number}");
+
 
 loop {    
     println!("Please input your guess.");
